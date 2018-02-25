86a104010fce54baba05327f4f682c553979a321
Houssam Sawan <eng.houssam.sawan@gmail.com>
2018-02-25 16:56:44 +0300

    complete the view method in pages class

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,6 +3,20 @@
         
         public function view($page = 'home'){
 
+
+            //Check if the requested page exisist
+            if( ! file_exists(APPPATH."/views/pages/". $page . ".php")){
+                //show not found page
+                show_404();
+            }
+
+
+            $data['title'] = ucfirst($page); // Capitalize the first letter
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/'.$page , $data);
+            $this->load->view('templates/footer', $data);
+
         }
     }
 ?>
