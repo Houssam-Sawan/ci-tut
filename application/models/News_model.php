ac79a9fcb70dbb477d265bd246d8faeca86586fc
Houssam Sawan <eng.houssam.sawan@gmail.com>
2018-02-26 11:48:37 +0300

    Bug fix: no query variable was defined and no echo statment befor news item title

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -15,11 +15,11 @@
 
         if ($slug === False)
         {
-            $this->db->get('news');
+            $query = $this->db->get('news');
             return $query->result_array();
         }
 
-        $this->db->get_where('news', array( 'slug' => $slug));
+        $query = $this->db->get_where('news', array( 'slug' => $slug));
         return $query->row_array();
 
     }
--- application/views/news/index.php
+++ application/views/news/index.php
@@ -2,17 +2,18 @@
 
 <?php
 
-    foreach ($news as $news_entry):?>
+    foreach ($news as $news_item):?>
 
-        <h3> <?php $news_entry['title'];?> </h3>
+    <h2>News Today</h2>
+        <h3> <?php echo $news_item['title'];?> </h3>
 
         <div class="main">
 
-            <?php echo $news_entry['text']; ?>
+            <?php echo $news_item['text']; ?>
         
         </div>
 
-        <p><a href="<?php echo site_url('news/'.$news_entry['slug']); ?> "> View Article</a></p>
+        <p><a href="<?php echo site_url('news/'.$news_item['slug']); ?> "> View Article</a></p>
 
     <?php endforeach ?>    
 
