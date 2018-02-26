2dd67a7e5a3bb1a22bd928e532538b76090c656e
Houssam Sawan <eng.houssam.sawan@gmail.com>
2018-02-26 09:46:16 +0300

    add get_news method to retrive news data from news table in the database based on slug

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -8,6 +8,21 @@
 
         }
 
+    
+
+
+    public function get_news($slug = False) {
+
+        if ($slug === False)
+        {
+            $this->db->get('news');
+            return $query->result_array();
+        }
+
+        $this->db->get_where('news', array( 'slug' => $slug));
+        return $query->row_array();
+
     }
+}
 
 ?>
